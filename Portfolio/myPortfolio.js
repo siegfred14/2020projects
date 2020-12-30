080790f769c28078bb5ecb06169412617f269bb3
Siegfred Samson <siegfredsamson@gmail.com>
2020-12-30 11:37:15 +0100

    modified myPorfolio.js

--- Portfolio/myPortfolio.js
+++ Portfolio/myPortfolio.js
@@ -1,8 +1,10 @@
 let wrapperEl = document.querySelector(".wrapper");
 let mainEl = document.querySelector(".main-content");
 let submitEl = document.querySelector(".submit");
+let anchorEl = document.querySelector(".nav-list");
 
 submitEl.addEventListener("click", function(){
-    mainEl.classList.remove(".main-edited");
-    wrapperEl.classList.add(".main-edited")
-})
+    mainEl.classList.remove("main-edited");
+    wrapperEl.classList.add("main-edited");
+    anchorEl.classList.remove("nav-list2");
+});
